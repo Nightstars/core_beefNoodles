32bf6ce963cc7796e8a99501318cb778882324b7
Nightstars <christzhangowner#gmail.com>
2020-10-31 16:54:24 +0800

    add view

---
+++ core_beefNoodles/Controllers/Test.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Threading.Tasks;
+
+namespace core_beefNoodles.Controllers
+{
+   // public class TestController
+   //[Controller]
+   // public class Test
+    public class Test: Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("Hello From Test Index");
+        }
+
+        public string About()
+        {
+            return "Hello From Test About";
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
