b6d656fc18ccfc8c4fbb1a1f0febdacf3ff07b36
Nightstars <christzhangowner#gmail.com>
2020-11-01 20:18:17 +0800

    add EF core

---
+++ core_beefNoodles/Migrations/20201101121212_NoodleChanges.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace core_beefNoodles.Migrations
+{
+    public partial class NoodleChanges : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsInstock",
+                table: "Noodles",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsInstock",
+                table: "Noodles");
+        }
+    }
+}
